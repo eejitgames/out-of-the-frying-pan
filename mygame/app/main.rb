d41a3798ca15db78d79a88cbea20227fba493b0c
eejit <eejitgames@gmail.com>
2024-08-28 12:12:50 +0100

    simplified implementation, main.rb only

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,23 +1,100 @@
-require "app/input.rb"
-require "app/sprite.rb"
-require "app/util.rb"
+include AttrGTK
 
-require "app/constants.rb"
-require "app/menu.rb"
-require "app/scene.rb"
-require "app/game_setting.rb"
-require "app/sound.rb"
-require "app/text.rb"
-require "app/player.rb"
+module ToggleExtension
+  def toggle
+    putz "the console was toggled"
+    super
+  end
+end
 
-require "app/scenes/gameplay.rb"
-require "app/scenes/main_menu.rb"
-require "app/scenes/paused.rb"
-require "app/scenes/settings.rb"
+GTK::Console.prepend ToggleExtension
 
-require "app/scenes/ramps.rb"
+def boot args
+  args.state = nil
+end
 
-require "app/frying-pan/main.rb"
+def reset args
+  state.game = nil
+end
 
-# NOTE: add all requires above this
-require "app/tick.rb"
+def tick args
+  args.state ||= {}
+  self.args = args
+  state.game ||= {
+    current_scene: :title_scene,
+    clock: 0
+  }
+
+  current_scene = state.game.current_scene
+  outputs.debug.watch state.game
+
+  send(current_scene)
+
+  if state.game.next_scene
+    state.game.current_scene = state.game.next_scene
+    state.game.next_scene = nil
+  end
+end
+
+def title_scene
+  outputs.labels << { x: 640, y: 40, text: "Title Scene (click to go to game)", alignment_enum: 1 }
+
+  return if game_has_lost_focus?
+
+  if inputs.mouse.click
+    state.game.next_scene = :game_scene
+  end
+end
+
+def game_scene
+
+=begin
+    input
+    calc
+    render
+=end
+
+  render_array = []
+  render_array << { x: 0, y: 0, w: 1280, h: 720, path: "sprites/fuds.png" }
+  render_array << { x: 640, y: 40, text: "Game Scene (click to go to game over)", alignment_enum: 1 }
+  render_array << { x: 1195, y: 640, w: 128 / 2, h: 101 / 2, path: 'dragonruby.png', angle: state.game.clock }
+  outputs.primitives << render_array
+
+  return if game_has_lost_focus?
+
+  state.game.clock += 1
+
+  if inputs.mouse.click
+    state.game.next_scene = :game_over_scene
+  end
+end
+
+def game_over_scene
+  outputs.labels << { x: 640, y: 40, text: "Game Over Scene (click to go to title)", alignment_enum: 1 }
+
+  return if game_has_lost_focus?
+
+  if inputs.mouse.click
+    state.game.next_scene = :title_scene
+  end
+end
+
+def game_has_lost_focus?
+  return true if Kernel.tick_count < 1
+
+  current_focus = !inputs.keyboard.has_focus
+
+  if current_focus != state.game.lost_focus
+    if current_focus
+      # puts "lost focus"
+      # audio[:music].paused = true
+    else
+      # puts "gained focus"
+      # audio[:music].paused = false
+    end
+  end
+  state.game.lost_focus = current_focus
+end
+
+$gtk.reset
+$gtk.disable_framerate_warning!
